6e0134a5f320a6f33cf801d3e815a1aea6a3a236
Grégoire Pineau <lyrixx@lyrixx.info>
2013-08-24 23:26:35 +0200

    Added support for TOC

--- src/HtmlTools/Helpers.php
+++ src/HtmlTools/Helpers.php
@@ -42,4 +42,101 @@ class Helpers
         // Remove \DomDocument's extra tags (doctype, html, body). Yeah, that's (a bit) ugly.
         return preg_replace('#.*<html><body>(.*)</body></html>.*#is', '\1', $document->saveHTML());
     }
+
+    /**
+     * Build a Table Of Content
+     *
+     * @static
+     * @param string $html html string
+     * @return array
+     */
+    public static function buildTOC($html)
+    {
+        if (!$html) {
+            return array();
+        }
+
+        $html = static::addHeadingsId($html, 'h1, h2, h3, h4, h5, h6');
+
+        $document = new \DOMDocument();
+        $document->loadHTML('<?xml encoding="UTF-8">'.$html);
+        $xpath = new \DOMXPath($document);
+
+        $toc = array();
+        $h1 = $h2 = $h3 = $h4 = $h5 = $h5 = $h6 = 0;
+        foreach ($xpath->query(CssSelector::toXPath('h1, h2, h3, h4, h5, h6')) as $node) {
+            $nodeName = $node->nodeName;
+            $title = $node->nodeValue;
+            $id = $node->getAttribute('id');
+            switch ($nodeName) {
+                case 'h1':
+                    $toc[++$h1] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+
+                case 'h2':
+                    $toc = self::fixToc($toc, $nodeName, $h1, $h2, $h3, $h4, $h5, $h5);
+                    $toc[$h1]['childs'][++$h2] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+
+                case 'h3':
+                    $toc = self::fixToc($toc, $nodeName, $h1, $h2, $h3, $h4, $h5, $h5);
+                    $toc[$h1]['childs'][$h2]['childs'][++$h3] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+
+                case 'h4':
+                    $toc = self::fixToc($toc, $nodeName, $h1, $h2, $h3, $h4, $h5, $h5);
+                    $toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][++$h4] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+
+                case 'h5':
+                    $toc = self::fixToc($toc, $nodeName, $h1, $h2, $h3, $h4, $h5, $h5);
+                    $toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4]['childs'][++$h5] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+
+                case 'h6':
+                    $toc = self::fixToc($toc, $nodeName, $h1, $h2, $h3, $h4, $h5, $h5);
+                    $toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4]['childs'][$h5]['childs'][++$h6] = array('title' => $title, 'id' => $id, 'childs' => array());
+                break;
+            }
+        }
+
+        return $toc;
+    }
+
+    private static function fixToc($toc, $level, $h1, $h2, $h3, $h4, $h5)
+    {
+        if (!isset($toc[$h1])) {
+            $toc[$h1] = array('title' => null, 'id' => null, 'childs' => array());
+        }
+        if ('h2' == $level) {
+            return $toc;
+        }
+
+        if (!isset($toc[$h1]['childs'][$h2])) {
+            $toc[$h1]['childs'][$h2] = array('title' => null, 'id' => null, 'childs' => array());
+        }
+        if ('h3' == $level) {
+            return $toc;
+        }
+
+        if (!isset($toc[$h1]['childs'][$h2]['childs'][$h3])) {
+            $toc[$h1]['childs'][$h2]['childs'][$h3] = array('title' => null, 'id' => null, 'childs' => array());
+        }
+        if ('h4' == $level) {
+            return $toc;
+        }
+
+        if (!isset($toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4])) {
+            $toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4] = array('title' => null, 'id' => null, 'childs' => array());
+        }
+        if ('h5' == $level) {
+            return $toc;
+        }
+
+        if (!isset($toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4]['childs'][$h5])) {
+            $toc[$h1]['childs'][$h2]['childs'][$h3]['childs'][$h4]['childs'][$h5] = array('title' => null, 'id' => null, 'childs' => array());
+        }
+
+        return $toc;
+    }
 }
--- tests/HtmlTools/Tests/HelpersTest.php
+++ tests/HtmlTools/Tests/HelpersTest.php
@@ -5,8 +5,6 @@ namespace HtmlTools\Tests;
 use HtmlTools\Helpers;
 
 /**
- *
- *
  * @author Yohan Giarelli <yohan@frequence-web.fr>
  */
 class HelpersTest extends \PHPUnit_Framework_TestCase
@@ -31,5 +29,130 @@ class HelpersTest extends \PHPUnit_Framework_TestCase
             array('<p>test</p>', '<p>test</p>'),
             array('<ul><li>foo</li><li>bar</li></ul>', '<ul><li>foo<li>bar</ul>'),
         );
+    }
+
+    public function testBuildTOC()
+    {
+        $document = <<<EOL
+<h1>1.0.0.0.0.0</h1>
+<h2>1.1.0.0.0.0</h2>
+<h2>1.2.0.0.0.0</h2>
+<h3>1.2.1.0.0.0</h3>
+<h3>1.2.2.0.0.0</h3>
+<h4>1.2.2.1.0.0</h4>
+<h5>1.2.2.1.1.0</h5>
+<h6>1.2.2.1.1.1</h6>
+<h1>Another Title</h1>
+EOL;
+        $expected = array(
+            1 => array(
+                'title' => '1.0.0.0.0.0',
+                'id' => '1-0-0-0-0-0',
+                'childs' => array(
+                    1 => array(
+                        'title' => '1.1.0.0.0.0',
+                        'id' => '1-1-0-0-0-0',
+                        'childs' => array(),
+                    ),
+                    2 => array(
+                        'title' => '1.2.0.0.0.0',
+                        'id' => '1-2-0-0-0-0',
+                        'childs' => array(
+                            1 => array(
+                                'title' => '1.2.1.0.0.0',
+                                'id' => '1-2-1-0-0-0',
+                                'childs' => array(),
+                            ),
+                            2 => array(
+                                'title' => '1.2.2.0.0.0',
+                                'id' => '1-2-2-0-0-0',
+                                'childs' => array(
+                                    1 => array(
+                                        'title' => '1.2.2.1.0.0',
+                                        'id' => '1-2-2-1-0-0',
+                                        'childs' => array(
+                                            1 => array(
+                                                'title' => '1.2.2.1.1.0',
+                                                'id' => '1-2-2-1-1-0',
+                                                'childs' => array(
+                                                    1 => array(
+                                                        'title' => '1.2.2.1.1.1',
+                                                        'id' => '1-2-2-1-1-1',
+                                                        'childs' => array(),
+                                                    ),
+                                                ),
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            2 => array(
+                'title' => 'Another Title',
+                'id' => 'another-title',
+                'childs' => array(
+                )
+            ),
+        );
+        $this->assertSame($expected, Helpers::buildTOC($document));
+    }
+
+    public function testBuildTOCWithWeirdDOM()
+    {
+        $document = <<<EOL
+<h2>h2</h2>
+<h3>h3</h3>
+<h2>h2</h2>
+<h4>h4</h4>
+<h3>h3</h3>
+EOL;
+        $expected = array(
+            0 => array(
+                // H1 supposed to be here
+                'title' => null,
+                'id' => null,
+                'childs' => array(
+                    1 => array(
+                        'title' => 'h2',
+                        'id' => 'h2',
+                        'childs' => array(
+                            1 => array(
+                                'title' => 'h3',
+                                'id' => 'h3',
+                                'childs' => array(),
+                            ),
+                        ),
+                    ),
+                    2 => array(
+                        'title' => 'h2',
+                        'id' => 'h2-2',
+                        'childs' => array(
+                            1 => array(
+                                // H2 supposed to be here
+                                'title' => null,
+                                'id' => null,
+                                'childs' => array(
+                                    1 => array(
+                                        'title' => 'h4',
+                                        'id' => 'h4',
+                                        'childs' => array(),
+                                    ),
+                                ),
+                            ),
+                            2 => array(
+                                'title' => 'h3',
+                                'id' => 'h3-2',
+                                'childs' => array(),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $this->assertSame($expected, Helpers::buildTOC($document));
     }
 }
